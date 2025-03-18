dc910274550b2a9e2a03943961a2b232764dadd1
FIC <fic@info.local>
2025-03-17 21:57:27 -0300

    Revolucao industrial da era da pedra

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿//for (int i = 0; i <= 20; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+//int senha = 0;
+
+
+//while (senha != 1234)
+//{
+//    Console.WriteLine("Digite a senha 1234: ");
+//    senha = int.Parse(Console.ReadLine());
+
+//}
+
+//Console.WriteLine("Escolha um numero para ver sua tabuada: ");
+//int numero = int.Parse(Console.ReadLine());
+
+//Console.Clear();
+
+//for (int i = 1; i <= 10; i++)
+//{
+//    Console.WriteLine(numero*i);
+//}
+
+//Console.WriteLine("Digite um numero: ");
+//int i = int.Parse(Console.ReadLine());
+
+//for (; i >= 0; i -= 1)
+//{
+//    Console.WriteLine(i);
+//}
+
+int nota = 0;
+int media = 0;
+
+while (nota <= 10 && nota >= 0)
+{
+    
+}
+
+
+
+//  I W B B
