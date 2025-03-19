ca16cef4996d635f5691d0295660b681fe399a30
FIC <fic@info.local>
2025-03-18 21:50:29 -0300

    a revolucao chegou

--- Program.cs
+++ Program.cs
@@ -31,14 +31,116 @@
 //    Console.WriteLine(i);
 //}
 
-int nota = 0;
-int media = 0;
+//int nota = 0;
+//int media = 0;
+//int contagem = 0;
 
-while (nota <= 10 && nota >= 0)
+//while (nota <= 10 && nota >= 0)
+//{
+//    Console.WriteLine("Digite um numero ate errar");
+//    nota = int.Parse(Console.ReadLine());
+
+//    if (nota <= 10 && nota >= 0)
+//    {
+//        contagem += nota;
+//        media += 1;
+//    }
+
+//}
+
+//Console.WriteLine($"media das notas {contagem / media}");
+
+//int numero;
+//int contar = 0;
+//int somar = 0;
+
+//while (true)
+//{
+//    Console.WriteLine("Digite um numero ate errar");
+//    numero = int.Parse(Console.ReadLine());
+
+//    if (numero >= 0 && numero <= 10)
+//    {
+//        contar++;
+//        somar += numero;
+//    }
+//    else
+//    {
+//        break;
+//    }
+//}
+
+//Console.WriteLine($"a media das suas tentativas é {somar/contar}");
+
+//int numero;
+//int contagem = 0;
+
+//while(true)
+//{
+//    Console.WriteLine("Digite um numero");
+//    numero = int.Parse(Console.ReadLine());
+
+//    if (numero > 0)
+//    {
+//        contagem += numero;
+//    }
+//    else
+//    {
+//        break;
+//    }
+
+//}
+
+//Console.WriteLine($"A soma dos seus valores é {contagem}");
+
+//Console.WriteLine("Digite um numero e vou verificar se é primo");
+//int primo = int.Parse(Console.ReadLine());
+//int contador = primo-1;
+//int verif = 0;
+
+//while (contador > 0)
+//{
+//    verif = primo % contador;
+
+//    if (verif == 0)
+//    {
+//        Console.WriteLine("Seu numero nao é primo");
+//        break;
+//    }
+
+//    contador -= 1;
+//    Console.WriteLine(contador);
+
+//    if (contador == 1)
+//    {
+//        Console.WriteLine("Seu numero é primo");
+//        break;
+//    }
+
+//}
+int idade = 0;
+while (true)
 {
-    
+	try
+	{
+		Console.WriteLine("Digite sua idade");
+		idade = int.Parse(Console.ReadLine());
+		Console.WriteLine(idade);
+		break;
+	}
+	catch (ArgumentNullException)
+	{
+		Console.WriteLine("voce digitou um valor vazio");
+	}
+	catch (FormatException)
+	{
+		Console.WriteLine("permitido apenas numero");
+	}
+	catch (OverflowException)
+	{
+		Console.WriteLine("voce digitou um valor muito grande");
+	}
+
 }
-
-
 
 //  I W B B
--- obj/Debug/net9.0/EraDaPedra.AssemblyInfo.cs
+++ obj/Debug/net9.0/EraDaPedra.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EraDaPedra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc910274550b2a9e2a03943961a2b232764dadd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("EraDaPedra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EraDaPedra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
